676705348b7494549f9cb339184b2c57073656d7
29071 <lovestudy@qq.com>
2024-05-26 10:15:12 +0800

    otel理解

--- src/storage/redis/redis.go
+++ src/storage/redis/redis.go
@@ -11,17 +11,18 @@ var Client redis.UniversalClient
 
 func init() {
 	addrs := strings.Split(config.EnvCfg.RedisAddr, ";")
+	// Universal 通用的
 	Client = redis.NewUniversalClient(&redis.UniversalOptions{
 		Addrs:      addrs,
 		Password:   config.EnvCfg.RedisPassword,
 		DB:         config.EnvCfg.RedisDB,
 		MasterName: config.EnvCfg.RedisMaster,
 	})
-
+	// todo 未设置链路追踪服务商
 	if err := redisotel.InstrumentTracing(Client); err != nil {
 		panic(err)
 	}
-
+	// todo 未设置检测踪服务商
 	if err := redisotel.InstrumentMetrics(Client); err != nil {
 		panic(err)
 	}
